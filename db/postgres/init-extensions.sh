a883fc733720c9607feb4039b25b8fa72c8244bf
webmasha <i.mary.efimova@gmail.com>
2026-04-22 11:40:10 +0500

    fix: add LOAD age and SET search_path to init script
    - Add LOAD 'age' directive for Apache AGE initialization
    - Set search_path to ag_catalog for proper cypher query support

--- db/postgres/init-extensions.sh
+++ db/postgres/init-extensions.sh
@@ -4,4 +4,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-voproshalych}" --dbname "${POSTGRES_DB:-voproshalych}" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS vector;
     CREATE EXTENSION IF NOT EXISTS age;
+    LOAD 'age';
+    SET search_path = ag_catalog, "\$user", public;
 EOSQL
